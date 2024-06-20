aafb6167ff1f0a2794ec2d7b18e5f9423cf1863c
Everyday-hiro <hiromu00815@gmail.com>
2024-06-20 21:30:58 +0900

    登録フォームに名前を出せるようになったが、まだレビューを投稿できないです。

--- kadai_tutorial_output/src/main/java/com/example/samuraitravel/service/ReviewService.java
+++ kadai_tutorial_output/src/main/java/com/example/samuraitravel/service/ReviewService.java
@@ -4,37 +4,29 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.samuraitravel.entity.House;
 import com.example.samuraitravel.entity.Review;
 import com.example.samuraitravel.entity.User;
 import com.example.samuraitravel.form.ReviewEditForm;
 import com.example.samuraitravel.form.ReviewRegisterForm;
-import com.example.samuraitravel.repository.HouseRepository;
 import com.example.samuraitravel.repository.ReviewRepository;
-import com.example.samuraitravel.repository.UserRepository;
 import com.example.samuraitravel.security.UserDetailsImpl;
 
 @Service
 public class ReviewService {
 
 	private final ReviewRepository reviewRepository;
-	private final HouseRepository houseRepository;
-	private final UserRepository userRepository;
 	
-	public ReviewService(ReviewRepository reviewRepository, HouseRepository houseRepository, UserRepository userRepository) {
+	public ReviewService(ReviewRepository reviewRepository) {
 		this.reviewRepository = reviewRepository;
-		this.houseRepository = houseRepository;
-		this.userRepository =  userRepository;
+
 	}
 	
 	@Transactional
 	public void create(ReviewRegisterForm reviewRegisterForm, @AuthenticationPrincipal UserDetailsImpl userDetailsImpl) {
 		Review review = new Review();
-		House house = houseRepository.getReferenceById(reviewRegisterForm.getHouseId());
 		User user = userDetailsImpl.getUser();
 		
-		review.setHouseId(house);
-		review.setName(reviewRegisterForm.getName());
+		user.setName(userDetailsImpl.getUsername());
 		review.setStar(reviewRegisterForm.getStar());
 		review.setExplanation(reviewRegisterForm.getExplanation());
 		
